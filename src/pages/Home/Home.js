27da0110a5acd2ee520abd2e65992e7eab8ed6eb
karanjoshi1206 <joshikaran392@gmail.com>
2023-04-30 18:02:42 +0530

    minor change

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
 	const checkUserLoggedIn = localStorage.getItem("user");
 	console.log(JSON.parse(checkUserLoggedIn));
-	return <div>Home</div>;
+	return <div>Still in development</div>;
 };
 
 export default Home;
